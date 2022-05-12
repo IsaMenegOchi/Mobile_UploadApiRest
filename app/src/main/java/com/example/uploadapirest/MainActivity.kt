931cf506d2bef30a11187bbd7da211ec769ab8c8
Isabelle M. Ochini <isabelle.ochini@gmail.com>
2022-05-12 10:45:09 -0300

    Realizando mudanças de imagem ao selecionar no local de fotos

--- app/src/main/java/com/example/uploadapirest/MainActivity.kt
+++ app/src/main/java/com/example/uploadapirest/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.example.uploadapirest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-
-    //    public void openGalery(){
-//
-//        //criamos uma variavel com
-//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//
-//        //qual o tipo de recurso que quero pegar
-//        intent.setType("image/*");
-//
-//        //abrir a activity responsavel por exibir as imagens, na qual retornará o conteudo selecionado para o nosso app
-//        this.startActivityForResult(Intent.createChooser(intent, "Escolha uma foto"), CODE_IMAGE);
-//    }
 }
